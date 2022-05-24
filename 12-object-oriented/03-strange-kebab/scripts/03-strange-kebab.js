cbdb698778e1ab3d8abc6acadd70efce564cdbbc
raul <raulbercea@gmail.com>
2022-05-24 11:10:43 +0200

    finished script of ex 03 unit 12

---
+++ 12-object-oriented/03-strange-kebab/scripts/03-strange-kebab.js
@@ -0,0 +1,37 @@
+/**
+ * @file: 03-strange-kebab.js
+ * @author: Raul Bercea
+ * Adding a method to the string object in javascript that modifies a string with regular expressions
+ * 
+ * in order to make the 
+ */
+
+// sample strings
+const source = [
+  "MyNameIsMyPassportVerifyMe",
+  "My Name Is My Passport Verify Me MMM",
+  " -- -My?Name&*is**my$$Passport???p??",
+  "mY--name--- is- - 2023---",
+  "mynameismypassport",
+  "2022 my name is",
+  "2024-my-name-is",
+];
+
+/**
+ * Function that will transform a string to a strange kebab string
+ */
+String.prototype.toStrangeKebab = function () {
+  let string = this.replace(/[^a-zA-Z0-9 ]/g, " ") // removing all symbols
+    .replace(/^[0-9]{4}/, "") // removing numbers at the begining
+    .replace(/[A-Z]/g, " $&") // adding a white space before capital letters
+    .replace(/\s\s+/g, " ") // replacing multiple whitespaces with one
+    .trim() // removing whitespaces at the begining of the string
+    .replace(/\s+/g, "-") // replacing spaces with dashes
+    .toLowerCase(); // making the string lowercase
+
+  return string;
+  c;
+};
+
+// running the function for all the sample strings
+source.forEach((item) => console.log(item.toStrangeKebab()));
